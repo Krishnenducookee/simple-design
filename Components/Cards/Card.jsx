ba5bafa384e24b2c609cb4ebcfb4dd9e8d55fb46
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-09-14 12:17:08 +0530

    Update Card.jsx

--- Components/Cards/Card.jsx
+++ Components/Cards/Card.jsx
@@ -1,14 +1,47 @@
-const Card=()=>{
-    return(
-        <div className='p-6 max-w-sm  bg-white rounded-xl flex items-center'>     
-            <p class="text-lg text-black font-semibold">
-            Lorem Ipsum is simply dummy text of the printing
-            and typesetting industry. Lorem Ipsum has been
-            the industry's standard dummy text ever since the
-            1500s, when an unknown printer took a galley of 
-            type and scrambled it.
-            </p> 
-           </div>
-    )
+import {useState } from "react";
+
+const Card = () => {
+  const [paragraph, setParagraph] = useState({
+    newText:[],
+    isOriginal: true,
+  });
+
+  const originalPara =
+    "Lorem Ipsum is simply dummy paragraph of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+const undo=()=>{
+
+   
 }
-export default Card
+
+  return (
+    <div className="p-6 md:max-w-xl bg-white rounded-xl items-center">
+      <button
+        className="rounded w-10 h-10 border bg-red-500"
+        onClick={undo}
+      >
+        undo
+      </button>
+      <button className="rounded w-10 h-10 border bg-green-500">redo</button>
+      <button
+        className="rounded-full w-5 h-5 border bg-red-500"
+        onClick={() => {
+          setParagraph({ isOriginal: true });
+        }}
+      ></button>
+      <p className="paragraph-lg paragraph-black font-semibold">
+        <textarea
+          className="h-[330px] resize-none"
+          onChange={(e) => {
+            setParagraph({ newText: e.target.value, isOriginal: false });
+          }} 
+          value={paragraph.isOriginal ? originalPara : paragraph.newText}
+        />
+      </p>
+    </div>
+  );
+};
+
+export default Card;
+
+
