7ce363697b44eed66d9e95f243d878367cccdad4
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-09-14 12:18:00 +0530

    Update SideBar.jsx

--- Components/Layouts/SideBar.jsx
+++ Components/Layouts/SideBar.jsx
@@ -1,15 +1,15 @@
 const SideBar=()=>{
 
-    const list=["Lorem ipsum","Consectetur","Exercitation","Ut enim","Quis nostrud ","Laboris nisi"]
+    const list=["Lorem ipsum","Consectetur ghcf","Exercitation cfbc","vchhgv","Quis nostrud ","Laboris nisi"]
 
     return (
-        <div>
+        <div className="pl-12">
             
-        <img className=" ml-8 mt-6 rounded-full w-10 h-10 md:w-20 md:h-20" src="/images/photo-1567446537708-ac4aa75c9c28.jpg"/>
-        <div className="text-white mt-12 ml-12">
-            <ul className="list-disc">
-               {list.map((data)=>(
-                <li>{data}</li>
+        <img className="mt-6 rounded-full w-10 h-10 md:w-20 md:h-20" src="/images/photo-1567446537708-ac4aa75c9c28.jpg"/>
+        <div className="text-white mt-12">
+            <ul className="list-disc list-outside flex flex-row md:flex-col">
+               {list.map((data, index)=>(
+                <li key={index}>{data}</li>
                ))}
             </ul>
             </div>
