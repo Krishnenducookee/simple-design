47c22a313ddafd779653b8068c2d16be5c016b79
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-09-14 12:19:30 +0530

    Update index.jsx

--- pages/card/index.jsx
+++ pages/card/index.jsx
@@ -1,15 +1,13 @@
-import Card from "@/Components/Cards/Card"
+import Card from "@/Components/Cards/Card";
 
-const Cards=()=>{
-    const componentArray=[]
-    for(let i=0;i<15;i++){
-        componentArray.push(<Card key={i}/>)
-    }
-    return (
-        <div className="-ml-24 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {componentArray}
-        </div>
-    )
-}
+const Cards = () => {
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+      {[...Array(15)].map((_, index) => {
+        return <Card key={index}/>;
+      })}
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
